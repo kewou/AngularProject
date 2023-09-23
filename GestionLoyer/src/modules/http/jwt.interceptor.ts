02d1676ce2257525adcf8bdc5848b285ef6d1876
noumia <kewou.noumia@gmail.com>
2023-09-23 23:47:05 +0200

    gestion token JWT

--- GestionLoyer/src/modules/http/jwt.interceptor.ts
+++ GestionLoyer/src/modules/http/jwt.interceptor.ts
@@ -1,28 +1,21 @@
 import { Injectable } from '@angular/core';
-import {HttpRequest,HttpHandler,HttpEvent,HttpInterceptor,} from '@angular/common/http';
+import {HttpRequest,HttpHandler,HttpEvent,HttpInterceptor,HttpResponse} from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { CookieService } from 'ngx-cookie-service';
 
 
 @Injectable()
-export class JwtInterceptor implements HttpInterceptor {
-  constructor() {}
+export class JwtInterceptor implements HttpInterceptor{
 
-  intercept(
-    request: HttpRequest<any>,
-    next: HttpHandler
-  ): Observable<HttpEvent<any>> {
-    // Récupérer le JWT depuis le stockage local, le sessionStorage ou un cookie
-    const jwt = localStorage.getItem('jwt'); // Modifiez en fonction de votre méthode de stockage
+  constructor(private cookieService: CookieService) {
 
-    // Si le JWT est présent, l'ajouter à l'en-tête de la requête
-    if (jwt) {
-      request = request.clone({
-        setHeaders: {
-          Authorization: `Bearer ${jwt}`,
-        },
-      });
-    }
+  }
 
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    //const localToken = localStorage.getItem("jwtToken");
+    const localToken = this.cookieService.get('jwtToken');
+    request = request.clone({headers: request.headers.set('Authorization','Bearer ' + localToken)});
     return next.handle(request);
   }
+
 }
