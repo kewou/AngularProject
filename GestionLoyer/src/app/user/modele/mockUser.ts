bb0505bc043adc1c7515b13e2e58015d2a089ed2
noumia <kewou.noumia@gmail.com>
2024-02-20 19:37:31 +0100

    clean

--- GestionLoyer/src/app/user/modele/mockUser.ts
+++
@@ -1,9 +0,0 @@
-import { User } from './user';
-
-export const USERS: User[] = [
-  /*
-  { name: 'NOUMIA',lastname:'Joel' },
-  { name: 'KOUMBA',lastname:'Alicia' },
-  { name: 'KOKO',lastname:'Freddy' }
-  */
-];
--- GestionLoyer/src/app/user/modele/user.ts
+++
@@ -1,7 +0,0 @@
-export interface User {
-  name: string;
-  lastName: string;
-  email: string;
-  phone: string;
-  password: string
-}
