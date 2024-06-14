54306f7fb5d72aa97d94e8f0100b227a71cf869f
CNANTCHOUANG <cnantchouang-ext@nexity.fr>
2024-06-14 18:37:36 +0200

    Update connexion form - validate inscription

--- src/app/connexion/connexion.component.ts
+++ src/app/connexion/connexion.component.ts
@@ -1,54 +1,81 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
 import { Router } from '@angular/router';
 import { environment } from '../../environments/environment';
 import { UserService } from '../user/service/user.service';
 import { CookieService } from 'ngx-cookie-service';
-import { throwError } from 'rxjs';
 
 @Component({
-  selector: 'app-connexion',
-  templateUrl: './connexion.component.html',
-  styleUrls: ['./connexion.component.scss']
+    selector: 'app-connexion',
+    templateUrl: './connexion.component.html',
+    styleUrls: ['./connexion.component.scss']
 })
-export class ConnexionComponent {
+export class ConnexionComponent implements OnInit{
 
 
-  loginObj: any = {username:"", password:""};
+    loginObj: any = {username:"", password:""};
 
-  private backendUrl = environment.backendUrl;
-  errorMessage: string | null = null;
+    private backendUrl = environment.backendUrl;
 
-  constructor(private http: HttpClient,private router: Router,private userService: UserService,private cookieService: CookieService,
+    href: string = ""
+    errorMessage: string | null = null;
+
+    httpOptions = {
+        headers: new HttpHeaders({
+            'Content-Type': 'application/json'
+        })
+    };
+
+    constructor(private http: HttpClient,private router: Router,private userService: UserService,private cookieService: CookieService,
     ) {
 
-  }
-
-  loginSubmit() {
-    this.errorMessage = null;
-    if (this.loginObj.username==='' || this.loginObj.password==='') {
-      this.errorMessage = "Veuillez remplir tous les champs requis.";
-      return;
     }
-    const url = `${this.backendUrl}/authenticate`;
-    this.userService.loginUser(this.loginObj).subscribe(
-      (response:any) => {
-        console.log("response",response);
-        this.cookieService.set('jwtToken', response.jwtToken, undefined, undefined, undefined, true, 'Lax');
+
+    loginSubmit() {
+        this.errorMessage = null;
+        if (this.loginObj.username==='' || this.loginObj.password==='') {
+            this.errorMessage = "Veuillez remplir tous les champs requis.";
+            return;
+        }
+        const url = `${this.backendUrl}/authenticate`;
+        this.userService.loginUser(this.loginObj).subscribe(
+            (response:any) => {
+                console.log("response",response);
+                this.cookieService.set('jwtToken', response.jwtToken, undefined, undefined, undefined, true, 'Lax');
+                this.userService.connectOrDisconnect();
+                this.router.navigate(['/compte-user']);
+            },
+            (error:HttpErrorResponse) => {
+                if(error.status === 401){
+                    this.errorMessage="Login ou mot de passe incorrect !";
+                }
+            }
+        );
+    }
+
+    ngOnInit(){
+        this.href = this.router.url
+        if (this.href.includes('#')) {
+            let queryParams = this.href.split("#")
+            let infos = queryParams[1].split("/")
+            let accountParams={"reference": infos[0], "verificationToken": infos[1]}
+            this.http.post(`${this.backendUrl}/users/verify-account`, accountParams, this.httpOptions).subscribe(
+                (response: any) => {
+                    this.cookieService.set('jwtToken', response.jwtToken, undefined, undefined, undefined, true, 'Lax');
+                    this.userService.connectOrDisconnect();
+                    this.router.navigate(['/compte-user']);
+                },
+                error => {
+                    console.log("Error occured", error);
+
+                }
+            )
+        }
+    }
+
+
+    logoutSubmit(){
         this.userService.connectOrDisconnect();
-        this.router.navigate(['/compte-user']);
-      },
-      (error:HttpErrorResponse) => {
-          if(error.status === 401){
-            this.errorMessage="Login ou mot de passe incorrect !";
-          }
-      }
-    );
- }
-
-
- logoutSubmit(){
-  this.userService.connectOrDisconnect();
- }
+    }
 
 }
