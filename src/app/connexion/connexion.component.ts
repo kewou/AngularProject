cd4062d4eecd70a839270718d89f30feda7e5510
noumia <kewou.noumia@gmail.com>
2025-11-21 22:57:54 +0100

    (#60 Interception des erreurs du back)

--- src/app/connexion/connexion.component.ts
+++ src/app/connexion/connexion.component.ts
@@ -1,90 +1,106 @@
-import { Component, OnInit } from '@angular/core';
-import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
-import { Router } from '@angular/router';
-import { environment } from '../../environments/environment';
-import { UserService } from '../user/service/user.service';
-import { CookieService } from 'ngx-cookie-service';
-import { HttpService } from '../utils/httpService';
+import { Component, OnInit } from "@angular/core";
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from "@angular/common/http";
+import { Router } from "@angular/router";
+import { environment } from "../../environments/environment";
+import { UserService } from "../user/service/user.service";
+import { CookieService } from "ngx-cookie-service";
+import { HttpService } from "../utils/httpService";
 import { jwtDecode } from "jwt-decode";
 
 @Component({
-    selector: 'app-connexion',
-    templateUrl: './connexion.component.html',
-    styleUrls: ['./connexion.component.scss']
+  selector: "app-connexion",
+  templateUrl: "./connexion.component.html",
+  styleUrls: ["./connexion.component.scss"],
 })
-export class ConnexionComponent implements OnInit{
+export class ConnexionComponent implements OnInit {
+  loginObj: any = { username: "", password: "" };
 
+  private backendUrl = environment.backendUrl;
 
-    loginObj: any = {username:"", password:""};
+  href: string = "";
+  errorMessage: string | null = null;
 
-    private backendUrl = environment.backendUrl;
+  httpOptions = {
+    headers: new HttpHeaders({
+      "Content-Type": "application/json",
+    }),
+  };
 
-    href: string = ""
-    errorMessage: string | null = null;
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private userService: UserService,
+    private cookieService: CookieService,
+    readonly httpService: HttpService
+  ) {}
 
-    httpOptions = {
-        headers: new HttpHeaders({
-            'Content-Type': 'application/json'
-        })
-    };
-
-    constructor(private http: HttpClient,private router: Router,private userService: UserService,private cookieService: CookieService,
-        readonly httpService:HttpService
-    ) {
-
+  loginSubmit() {
+    this.errorMessage = null;
+    if (this.loginObj.username === "" || this.loginObj.password === "") {
+      this.errorMessage = "Veuillez remplir tous les champs requis.";
+      return;
     }
-
-
-
-    loginSubmit() {
-        this.errorMessage = null;
-        if (this.loginObj.username==='' || this.loginObj.password==='') {
-            this.errorMessage = "Veuillez remplir tous les champs requis.";
-            return;
-        }
-        const url = `${this.backendUrl}/authenticate`;
-        const token = this.cookieService.get('jwtToken');
-        const userReference = this.cookieService.get('userReference');
-        if(token){
-            this.cookieService.delete('jwtToken');
-        }
-        if(userReference){
-            this.cookieService.delete('jwtToken');
-        }
-        this.userService.loginUser(this.loginObj).subscribe(
-            (response:any) => {
-                console.log("response",response);
-                this.userService.saveUserAuthenticationInfo(response.jwtToken);
-                this.userService.redirectToRoleBasedPage(response.jwtToken);
-            },
-            (error:HttpErrorResponse) => {
-                this.errorMessage = this.httpService.getErrorMessage(error.status);
-            }
-        );
+    const url = `${this.backendUrl}/authenticate`;
+    const token = this.cookieService.get("jwtToken");
+    const userReference = this.cookieService.get("userReference");
+    if (token) {
+      this.cookieService.delete("jwtToken");
     }
-
-    ngOnInit() {
-        this.href = this.router.url
-        if (this.href.includes('#')) {
-            let queryParams = this.href.split("#")
-            let infos = queryParams[1].split("/")
-            let accountParams={"reference": infos[0], "verificationToken": infos[1]}
-            this.httpService.post(`users/verify-account`, accountParams).subscribe(
-                (response: any) => {
-                    this.cookieService.set('jwtToken', response.jwtToken, undefined, undefined, undefined, true, 'Lax');
-                    this.router.navigate(['connexion']);
-                },
-                error => {
-                    console.log("Error occured", error);
-
-                }
-            )
+    if (userReference) {
+      this.cookieService.delete("jwtToken");
+    }
+    this.userService.loginUser(this.loginObj).subscribe(
+      (response: any) => {
+        console.log("response", response);
+        this.userService.saveUserAuthenticationInfo(response.jwtToken);
+        this.userService.redirectToRoleBasedPage(response.jwtToken);
+      },
+      (error: HttpErrorResponse) => {
+        // Vérifier si le compte n'est pas vérifié
+        if (error.error && error.error.message === "ACCOUNT_NOT_VERIFIED") {
+          this.errorMessage =
+            "Votre compte n'a pas encore été vérifié. Veuillez vérifier votre boîte mail (y compris le dossier spam) et cliquer sur le lien de validation pour activer votre compte. Si vous n'avez pas reçu l'email, veuillez nous contacter.";
+        } else {
+          this.errorMessage = this.httpService.getErrorMessage(error.status);
         }
-    }
+      }
+    );
+  }
 
-    loginWithGoogle() {
-      const redirectUri = encodeURIComponent('http://localhost:4200/beezyApi/login/success');
-      window.location.href = `http://localhost:8090/beezyApi/oauth2/authorize/google?redirect_uri=${redirectUri}`;
+  ngOnInit() {
+    this.href = this.router.url;
+    if (this.href.includes("#")) {
+      let queryParams = this.href.split("#");
+      let infos = queryParams[1].split("/");
+      let accountParams = { reference: infos[0], verificationToken: infos[1] };
+      this.httpService.post(`users/verify-account`, accountParams).subscribe(
+        (response: any) => {
+          this.cookieService.set(
+            "jwtToken",
+            response.jwtToken,
+            undefined,
+            undefined,
+            undefined,
+            true,
+            "Lax"
+          );
+          this.router.navigate(["connexion"]);
+        },
+        (error) => {
+          console.log("Error occured", error);
+        }
+      );
     }
+  }
 
+  loginWithGoogle() {
+    const redirectUri = encodeURIComponent(
+      "http://localhost:4200/beezyApi/login/success"
+    );
+    window.location.href = `http://localhost:8090/beezyApi/oauth2/authorize/google?redirect_uri=${redirectUri}`;
+  }
 }
